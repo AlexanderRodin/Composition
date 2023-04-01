ff471447b460ed23e05bc2c63acce99912dfaf65
alexanderrodin <pass.9157@gmail.com>
2023-04-01 20:49:35 +0300

    add binding adapter for elements view

--- app/src/main/java/com/example/composition/domain/entity/GameSettings.kt
+++ app/src/main/java/com/example/composition/domain/entity/GameSettings.kt
@@ -10,9 +10,4 @@ data class GameSettings(
     val minCountOfRightAnswers: Int,
     val minPercentOfRightAnswers: Int,
     val gameTimeInSecond: Int,
-) : Parcelable {
-    val minCountOfRightAnswersString: String
-        get() = minCountOfRightAnswers.toString()
-    val minPercentOfRightAnswersString: String
-        get() = minPercentOfRightAnswers.toString()
-}
+) : Parcelable
